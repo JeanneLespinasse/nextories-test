1bdc3812656e6688107a8483a42361b849282c20
Jeanne L <lespinasse.jeanne@gmail.com>
2020-04-11 20:56:18 +0200

    Add the possibility to erase an object

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -25,28 +25,34 @@ export class AppComponent {
     errors = [];
     objectList = [];
 
+    round(value) {
+        return parseFloat(value.toFixed(1));
+    }
+
     addObject() {
         let data = this.form.value;
-        
+
         if (data.length >= 0 &&
             data.width >= 0 &&
             data.heigth >= 0 &&
             data.name != '') {
-            this.volume = (data.length * data.width * data.heigth) / 1000000;
-            this.totalVolume += this.volume;
+            this.volume = this.round((data.length * data.width * data.heigth) / 1000000);
+            this.totalVolume = this.round(this.volume + this.totalVolume);
             this.objectList.push({
-                name : data.name,
-                length : data.length,
-                width : data.width,
-                heigth : data.heigth,
-                volume : this.volume
+                name: data.name,
+                length: data.length,
+                width: data.width,
+                heigth: data.heigth,
+                volume: this.volume
             });
-            console.log('objectList', this.objectList);
         } else {
             this.errors.push('La hauteur, la largeur et la longueur doivent être supérieurs à 0.');
             this.errors.push('Le nom du meuble doit être renseigné.');
             this.errors.push('Merci de corriger la saisie.');
             console.log(this.errors);
         }
+    }
+    removeObject(index) {
+        this.totalVolume = this.round(this.totalVolume - this.objectList.splice(index, 1)[0].volume);
     }
 }
